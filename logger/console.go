08e4f986b3cd78da19ceef6ff1a8632967c30c62
ClorisMoQi <748709762@qq.com>
2022-04-01 00:08:21 -0700

    fix log parameter error

--- logger/console.go
+++ logger/console.go
@@ -8,51 +8,51 @@ import (
 )
 
 type Logger struct {
-    Lv LogLevel
+	Lv LogLevel
 }
 
 func NewLogger(lvstr string) *Logger {
-    lv := LogLvStr2Int(lvstr)
+	lv := LogLvStr2Int(lvstr)
 	return &Logger{
-        Lv: lv,
-    }
+		Lv: lv,
+	}
 }
 
-func (l *Logger) Debug(identifier string, format string, args...interface{}) {
-    l.log(ERROR, identifier, format, args)
+func (l *Logger) Debug(identifier string, format string, args ...interface{}) {
+	l.log(ERROR, identifier, format, args...)
 }
 
 func (l *Logger) Trace(identifier string, msg string) {
-    l.log(ERROR, identifier, msg)
+	l.log(ERROR, identifier, msg)
 }
 
 func (l *Logger) Info(identifier string, msg string) {
-    l.log(ERROR, identifier, msg)
+	l.log(ERROR, identifier, msg)
 }
 
 func (l *Logger) Warn(identifier string, msg string) {
-    l.log(ERROR, identifier, msg)
+	l.log(ERROR, identifier, msg)
 }
 
 func (l *Logger) Error(identifier string, msg string) {
-    l.log(ERROR, identifier, msg)
+	l.log(ERROR, identifier, msg)
 }
 
 func (l *Logger) Fatal(identifier string, msg string) {
-    l.log(FATAL, identifier, msg)
+	l.log(FATAL, identifier, msg)
 }
 
-func (l *Logger)log(lv LogLevel, identifier string, format string, args ...interface{}) {
-    if l.Lv <= lv {
-        msg := fmt.Sprintf(format, args...)
-        now := time.Now()
-        lvstr := LogLvInt2Str(lv)
-        pc, file, line, ok := runtime.Caller(1)
-        if !ok {
-            return
-        }
-        funcname := runtime.FuncForPC(pc).Name()
-        caller := file + ":" + strconv.Itoa(line)
-	    fmt.Printf("[%s] [%s], caller: %s, func:[%s] identifier: %s, message: \"%s\"\n", lvstr, now.Format("2006-01-02 15:04:05"), caller, funcname, identifier, msg)
-    }
+func (l *Logger) log(lv LogLevel, identifier string, format string, args ...interface{}) {
+	if l.Lv <= lv {
+		msg := fmt.Sprintf(format, args...)
+		now := time.Now()
+		lvstr := LogLvInt2Str(lv)
+		pc, file, line, ok := runtime.Caller(2)
+		if !ok {
+			return
+		}
+		funcname := runtime.FuncForPC(pc).Name()
+		caller := file + ":" + strconv.Itoa(line)
+		fmt.Printf("[%s] [%s] caller: %s, func:[%s] identifier: %s, message: %s\n", lvstr, now.Format("2006-01-02 15:04:05"), caller, funcname, identifier, msg)
+	}
 }
--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/ClorisMoQi/logger/logger"
-	_ "github.com/ClorisMoQi/logger/logger"
 )
 
 func main() {
 	// log.Fatal("1111111")
-    logger := logger.NewLogger("debug")
-    logger.Debug("identifier", "hahaha!!!")
+	logger := logger.NewLogger("debug")
+	id := 10010
+	name := "testLog"
+	logger.Debug("test", "This is a debug log, id: %d, name:%s\n", id, name)
 }
